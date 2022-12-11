f5dab2d071abd58f2e6e81196de37def038c26a8
PearceStrickland <78804764+PearceStrickland@users.noreply.github.com>
2022-12-10 19:16:18 -0500

    fixed index.js for other procedures

--- client/pages/other/index.js
+++ client/pages/other/index.js
@@ -13,22 +13,24 @@ export default function Index() {
 
             <Navbar/>
             <Wrapper>
-                <RedirectButton href={'/other/'}>Start Funding</RedirectButton>
-                <RedirectButton href={'/other/'}>Hire Employee</RedirectButton>
-                <RedirectButton href={'/other/'}>Fire Employee</RedirectButton>
-                <RedirectButton href={'/other/'}>Manage Service</RedirectButton>
-                <RedirectButton href={'/other/'}>Takeover Drone</RedirectButton>
-                <RedirectButton href={'/other/'}>Join Swarm</RedirectButton>
-                <RedirectButton href={'/other/'}>Lead Swarm</RedirectButton>
-                <RedirectButton href={'/other/'}>Load Drone</RedirectButton>
-                <RedirectButton href={'/other/'}>Refuel Drone</RedirectButton>
-                <RedirectButton href={'/other/'}>Fuel Required</RedirectButton>
-                <RedirectButton href={'/other/'}>Fly Drone</RedirectButton>
-                <RedirectButton href={'/other/'}>Purchase Ingredient</RedirectButton>
-                <RedirectButton href={'/other/'}>Remove Ingredient</RedirectButton>
-                <RedirectButton href={'/other/'}>Remove Drone</RedirectButton>
-                <RedirectButton href={'/other/'}>Remove Pilot Role</RedirectButton>
+                <RedirectButton href={'/other/start_funding'}>Start Funding</RedirectButton>
+                <RedirectButton href={'/other/hire_employee'}>Hire Employee</RedirectButton>
+                <RedirectButton href={'/other/fire_employee'}>Fire Employee</RedirectButton>
+                <RedirectButton href={'/other/manage_service'}>Manage Service</RedirectButton>
+                <RedirectButton href={'/other/takeover_drone'}>Takeover Drone</RedirectButton>
+                <RedirectButton href={'/other/join_swarm'}>Join Swarm</RedirectButton>
+                <RedirectButton href={'/other/leave_swarm'}>Lead Swarm</RedirectButton>
+                <RedirectButton href={'/other/load_drone'}>Load Drone</RedirectButton>
+                <RedirectButton href={'/other/refuel_drone'}>Refuel Drone</RedirectButton>
+                <RedirectButton href={'/other/fuel_required'}>Fuel Required</RedirectButton>
+                <RedirectButton href={'/other/fly_drone'}>Fly Drone</RedirectButton>
+                <RedirectButton href={'/other/purchase_ingredient'}>Purchase Ingredient</RedirectButton>
+                <RedirectButton href={'/other/remove_ingredient'}>Remove Ingredient</RedirectButton>
+                <RedirectButton href={'/other/remove_drone'}>Remove Drone</RedirectButton>
+                <RedirectButton href={'/other/remove_pilot_role'}>Remove Pilot Role</RedirectButton>
             </Wrapper>
+
+
         </div>
     )
 }
