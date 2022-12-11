40c3d963a2140ce47df89f3ef7a12decee3559cb
jbiancaniello <jmbiancaniello@gmail.com>
2022-12-11 17:09:33 -0500

    Delete fuel_required.js
    title

--- client/pages/other/fuel_required.js
+++
@@ -1,7 +0,0 @@
-import Head from "next/head";
-import Navbar from "../../components/Navbar";
-import {Wrapper} from "../../components/Wrapper";
-import {useState} from "react";
-import Field from "../../components/Field";
-import {Button} from "../../components/Button";
-import Axios from "axios";
